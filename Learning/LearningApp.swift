cea901c2d286b139c1ea0314b601641c0dbc2d84
jwan <jwanfaiisal@outlook.com>
2024-10-28 14:23:47 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Jwan Faisal Alameer on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
